cd9aeca2f6afb647f677908ecdd85d2ccbb43d90
mategaestudante <matheuspaixaodf@hotmail.com>
2023-04-04 21:32:12 -0300

    commit
    up

---
+++ js/index.js
@@ -0,0 +1,95 @@
+
+let sb = window.document.getElementById('side-bar-mobile')
+
+function abrir(){
+
+
+    sb.style.display = 'flex'
+
+
+
+}
+function fechar(){
+
+    sb.style.display = 'none'
+
+}
+
+let foto1 = window.document.getElementById('foto1')
+let foto2 = window.document.getElementById('foto2')
+let foto3 = window.document.getElementById('foto3')
+let foto4 = window.document.getElementById('foto4')
+let foto5 = window.document.getElementById('foto5')
+
+function entrar(){
+
+    foto1.src = 'imagens/021863BPA1.webp'
+
+}
+function sair(){
+
+    foto1.src = 'imagens/021863BP.webp'
+
+}
+function entrar2(){
+
+    foto2.src = 'imagens/0226507TA1.webp'
+
+}
+function sair2(){
+
+    foto2.src = 'imagens/0226507T.webp'
+
+}
+function entrar3(){
+
+    foto3.src = 'imagens/02188315A1.webp'
+
+}
+function sair3(){
+
+    foto3.src = 'imagens/02188315.webp'
+
+}
+function entrar4(){
+
+    foto4.src = 'imagens/02192515A1.webp'
+
+}
+function sair4(){
+
+    foto4.src = 'imagens/02192515.webp'
+
+}
+function entrar5(){
+
+    foto5.src = 'imagens/02281515A1.webp'
+
+}
+function sair5(){
+
+    foto5.src = 'imagens/02281515.webp'
+
+}
+function fechar2(){
+
+    sb.style.display = 'none'
+
+    
+
+}
+function fechar3(){
+
+    sb.style.display = 'none'
+
+}
+function fechar4(){
+
+    sb.style.display = 'none'
+
+}
+function fechar5(){
+
+    sb.style.display = 'none'
+
+}
